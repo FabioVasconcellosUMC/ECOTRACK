f234c25a3c842250a0be9ce33f0dcd697c1e8183
Micael <micaelolivera22@gmail.com>
2026-04-01 23:47:25 -0300

    feat: crud de empresas criado

---
+++ src/main/java/com/ecotrack/ecotrack_api/controller/EmpresaController.java
@@ -0,0 +1,38 @@
+package com.ecotrack.ecotrack_api.controller;
+
+import com.ecotrack.ecotrack_api.entity.Empresa;
+import com.ecotrack.ecotrack_api.service.EmpresaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/empresas")
+@RequiredArgsConstructor
+public class EmpresaController {
+
+    private final EmpresaService empresaService;
+
+    @GetMapping
+    public List<Empresa> listar() {
+        return empresaService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Empresa> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(empresaService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Empresa> salvar(@RequestBody Empresa empresa) {
+        return ResponseEntity.ok(empresaService.salvar(empresa));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        empresaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
