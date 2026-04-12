f3fa7e14865c92dd2b0b2364c9c2972712abe216
FabioVasconcellosUMC <11222100395@alunos.umc.br>
2026-04-12 12:22:50 -0300

    feat: tela de lotes adicionada e empresas corrigida

--- ecotrack-frontend/src/router/index.js
+++ ecotrack-frontend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import MainLayout from '../layouts/MainLayout.vue'
 import DashboardView from '../views/DashboardView.vue'
+import EmpresasView from '../views/EmpresasView.vue'
+import LotesView from '../views/LotesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +21,16 @@ const router = createRouter({
           path: 'dashboard',
           name: 'dashboard',
           component: DashboardView,
+        },
+        {
+          path: 'empresas',
+          name: 'empresas',
+          component: EmpresasView,
+        },
+        {
+          path: 'lotes',
+          name: 'lotes',
+          component: LotesView,
         }
       ]
     }
