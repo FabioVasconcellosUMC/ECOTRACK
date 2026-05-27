b0a20f12417290042b33d60cbb4a3b1e1fbd1379
FabioVasconcellosUMC <11222100395@alunos.umc.br>
2026-05-27 17:07:18 -0300

    refactor: add domain exception classes

---
+++ src/main/java/com/ecotrack/ecotrack_api/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.ecotrack.ecotrack_api.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
