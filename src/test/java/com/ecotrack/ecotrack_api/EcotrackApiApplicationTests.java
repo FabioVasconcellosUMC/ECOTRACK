37b6b2fbfcae38dbcc73b0f017fb647a0ae1aade
FabioVasconcellosUMC <11222100395@alunos.umc.br>
2026-05-27 17:14:11 -0300

    test: usa profile de teste no contexto spring

--- src/test/java/com/ecotrack/ecotrack_api/EcotrackApiApplicationTests.java
+++ src/test/java/com/ecotrack/ecotrack_api/EcotrackApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.ecotrack.ecotrack_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcotrackApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
