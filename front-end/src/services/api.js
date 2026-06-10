0c7a9ec1fe7cf7101d5d6703ad1f36b81c1e3a77
FabioVasconcellosUMC <11222100395@alunos.umc.br>
2026-06-10 10:38:05 -0300

    fix: atualiza URL padrao da API

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://ecotrack-d5i0.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://api-ecotrack.duckdns.org',
 })
 
 api.interceptors.request.use((config) => {
